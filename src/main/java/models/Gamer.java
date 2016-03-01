b373df233eea382834390a83416da90ec0ccd7c7
HouPoc <hou199488@gmail.com>
2016-02-29 16:45:07 -0800

    change day one
    initial deck , player and dealer.
    current tests passed

---
+++ src/main/java/models/Gamer.java
@@ -0,0 +1,13 @@
+package models;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Wenbo on 2/29/2016.
+ */
+public abstract class Gamer {
+    public java.util.List<java.util.List<Card>> holder = new ArrayList<>();
+    public Gamer () {
+
+    }
+}
